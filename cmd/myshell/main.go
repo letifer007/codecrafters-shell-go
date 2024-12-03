65e752d99742e98a80ed869a68448c3f44a47221
Anurag Singh <anurag.singh@macmillan.com>
2024-12-04 00:27:57 +0530

    feat: added echo builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,30 +4,41 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
-
 func main() {
+
+	const (
+		EXIT = "exit"
+		ECHO = "echo"
+	)
 	
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	
 		// Since the string returned by ReadString('\n') includes a trailing newline
-		command = command[:len(command) - 1]
+		input = strings.TrimSpace(input)
+
+		if strings.HasPrefix(input, ECHO) {
+			args := strings.TrimSpace(strings.Join(strings.Split(input, "echo"), ""))
+			fmt.Fprintln(os.Stdout, args)
+			continue
+		}
+
+		if strings.HasPrefix(input, EXIT) {
+			os.Exit(0)
+		}
 
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
 
-		if command == "exit 0" {
-			os.Exit(0)
+		if len(input) > 1 {
+			fmt.Fprintln(os.Stderr, input + ": command not found")
 		}
-
-		fmt.Println(command + ": command not found")
 	}
 }

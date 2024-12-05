62ac18aed951b54007fc69073ceccf8f11244641
Anurag Singh <anurag.singh@macmillan.com>
2024-12-05 23:46:58 +0530

    feat: added type builtin executable files

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -18,42 +18,46 @@ func main() {
 	
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-
+		paths := strings.Split(os.Getenv("PATH"), ":")
+		
 		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	
-		// Since the string returned by ReadString('\n') includes a trailing newline
-		input = strings.TrimSpace(input)
-		input_split := strings.SplitN(input, " ", 2)
-
-		if strings.HasPrefix(input, TYPE) {
-			if input_split[1] == EXIT {
-				fmt.Fprintln(os.Stdout, EXIT+BUILTIN)
-			} else if input_split[1] == ECHO {
-				fmt.Fprintln(os.Stdout, ECHO+BUILTIN)
-			} else if input_split[1] == TYPE {
-				fmt.Fprintln(os.Stdout, TYPE+BUILTIN)
-			} else {
-				fmt.Fprintln(os.Stdout, input_split[1] + ": not found")
-			}
-			continue
-		}
-
-		if strings.HasPrefix(input, ECHO) {
-			args := input_split[1]
-			fmt.Fprintln(os.Stdout, args)
-			continue
-		}
-
-		if strings.HasPrefix(input, EXIT) {
-			os.Exit(0)
-		}
-
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
+		// Since the string returned by ReadString('\n') includes a trailing newline
+		input = strings.TrimSpace(input)
 
-		if len(input) > 1 {
+		input_split := strings.SplitN(input, " ", 2)
+		cmd := input_split[0]
+		args := ""
+		if len(input_split) > 1 {
+			args = input_split[1]
+		}
+
+		if cmd == EXIT {
+			os.Exit(0)
+		} else if cmd == TYPE {
+			switch args {
+			case ECHO, TYPE, EXIT:
+				fmt.Fprintln(os.Stdout, args + " is a shell builtin")
+			default:
+				found := false
+				for _, path := range paths {
+					exec := path + "/" + args
+					if _, err := os.Stat(exec); err == nil {
+						fmt.Fprintln(os.Stdout, args + " is " + exec)
+						found = true
+						break
+					}
+				}
+				if !found {
+					fmt.Fprintln(os.Stderr, args + ": not found")
+				}
+			}
+		} else if cmd == ECHO {
+			fmt.Fprintln(os.Stdout, args)
+		} else {
 			fmt.Fprintln(os.Stderr, input + ": command not found")
 		}
 	}
